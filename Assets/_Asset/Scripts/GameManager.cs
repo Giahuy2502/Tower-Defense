9aea3ccaf16650aba8a9d98c51988f9ac28de7e8
Giahuy2502 <huy1kicm@gmail.com>
2025-06-30 11:28:09 +0700

    feat(gameplay): add singleton GameManager

---
+++ Assets/_Asset/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void StartGame()
+    {
+        
+    }
+
+    public void EndGame()
+    {
+        
+    }
+
+    public void PauseGame()
+    {
+        
+    }
+}
