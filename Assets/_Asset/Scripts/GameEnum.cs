135408ae9b6b817b4c4e7d163f217d055f33dfa7
Giahuy2502 <huy1kicm@gmail.com>
2025-07-06 18:54:43 +0700

    feat(System): Add new monsters

--- Assets/_Asset/Scripts/GameEnum.cs
+++ Assets/_Asset/Scripts/GameEnum.cs
@@ -2,7 +2,7 @@ namespace _Asset.Scripts.MyAsset
 {
     public enum GameState { Start, Game, End }
     public enum TowerType {Runeblast,}
-    public enum MonsterType {Minion,}
+    public enum MonsterType {Minion,Warrior,Rouge,Mage}
     public enum MonsterState {Normal,Die,}
     public enum EventName{}
 }
