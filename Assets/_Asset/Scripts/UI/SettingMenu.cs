04a8ab16d638fa1f0723e8535229437e763180e9
Giahuy2502 <huy1kicm@gmail.com>
2025-08-02 18:23:48 +0700

    feat(Settings): add options to settings menu

--- Assets/_Asset/Scripts/UI/SettingMenu.cs
+++ Assets/_Asset/Scripts/UI/SettingMenu.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using static GameUltis;
@@ -10,16 +12,39 @@ public class SettingMenu : MonoBehaviour
     [Header("Setting Panel")]
     [SerializeField] private GameObject settingPanel;
     [SerializeField] private Button closeBtn;
-    
+    [SerializeField] private Button tutorialBtn;
+    [SerializeField] private Button logoutBtn;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+    [Header("ICON")]
+    [SerializeField] private Image soundIcon;
+    [SerializeField] private Image musicIcon;
+    [SerializeField] private Sprite soundIconOn;
+    [SerializeField] private Sprite soundIconOff;
+    [SerializeField] private Sprite musicIconOn;
+    [SerializeField] private Sprite musicIconOff;
     private GameManager gameManager => GameManager.instance;
 
     private void Awake()
     {
         settingBtn.onClick.AddListener(OnSetting);
         closeBtn.onClick.AddListener(OnClose);
+        tutorialBtn.onClick.AddListener(OnTutorial);
+        logoutBtn.onClick.AddListener(OnLogout);
+        musicSlider.onValueChanged.AddListener(OnMusicSliderChanged);
+        sfxSlider.onValueChanged.AddListener(OnSfxSliderChanged);
         Hide(settingPanel);
     }
 
+    private void OnMusicSliderChanged(float value)
+    {
+        musicIcon.sprite = value <= 0 ? musicIconOff : musicIconOn;
+    }
+
+    private void OnSfxSliderChanged(float value)
+    {
+        soundIcon.sprite = value <= 0 ? soundIconOff : soundIconOn;
+    }
     private void OnSetting()
     {
         Debug.Log("Pause Panel OnPause");
@@ -32,4 +57,18 @@ public class SettingMenu : MonoBehaviour
         Hide(settingPanel);
         Show(buildTowerPanel);
     }
+
+    private void OnTutorial()
+    {
+        Debug.Log("Show Tutorial Panel");
+        Hide(settingPanel);
+        Show(buildTowerPanel);
+    }
+    private void OnLogout()
+    {
+        Debug.Log("Logout game");
+        Hide(settingPanel);
+        Show(buildTowerPanel);
+    }
+    
 }
